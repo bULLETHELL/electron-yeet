cdbe1233be8b3fb773f30ae69d675416707f8668
FunkyXive <andreasnordenhof@gmail.com>
2020-12-17 14:25:04 +0100

    added graphs

--- graph.js
+++ graph.js
@@ -1,13 +1,13 @@
 const z = require("zebras")
 const vegalite = require("vega-lite")
 const vegaEmbed = require("vega-embed")
-
-const dataframe = z.readCSV("./P217SebringGP.csv")
-const dataParsed = z.parseNums(['"Time', '"Throttle"'])
+const Chart = require("chart.js")
+const dataframe = z.readCSV("./P217SebringGP2.csv")
+const dataParsed = z.parseNums(['Time', 'Throttle'])
 //const time = z.getCol('"Time', parsedData)
 //console.log(z.head(5, parsedData))
 //console.log(z.getCol('""Throttle""', dataframe))
-
+/*
 let graph= {
     data: {values: dataParsed},
     width: 700,
@@ -20,3 +20,22 @@ let graph= {
   }
 
 vegaEmbed("#graph", graph)
+*/
+time = z.getCol('Time', dataframe)
+throttle = z.getCol('Throttle', dataframe)
+console.log(time)
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: time,
+    datasets: [
+      { 
+        data: throttle
+      }
+    ]
+  },
+  options: {
+      
+  }
+});
