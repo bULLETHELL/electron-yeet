909381379c1b18ed6cf4d735d80716da29c5a10b
bULLETHELL <polyswing7@hotmail.com>
2020-12-18 14:00:02 +0100

    open file

--- graph.js
+++ graph.js
@@ -2,39 +2,41 @@ const z = require("zebras")
 const vegalite = require("vega-lite")
 const vegaEmbed = require("vega-embed")
 const Chart = require("chart.js")
+const { ipcRenderer } = require("electron")
 
 
-const dataframe = z.readCSV("./P217SebringGP2.csv")
-const dataParsed = z.parseNums(['Time', 'Throttle'])
+ipcRenderer.on('print-file', (event, data) => {
+    console.log(data)
+    let dataframe = z.readCSV(filepath)
+    let dataParsed = z.parseNums(['Time', 'Throttle'])
 
-time = z.getCol('Time', dataframe)
-throttle = z.getCol('Throttle', dataframe)
-brake = z.getCol('Brake', dataframe)
-fuelInLitres = z.getCol('Fuel Level', dataframe)
+    time = z.getCol('Time', dataframe)
+    throttle = z.getCol('Throttle', dataframe)
+    brake = z.getCol('Brake', dataframe)
+    fuelInLitres = z.getCol('Fuel Level', dataframe)
 
-function drawGraph(title, xAxis, yAxis, canvasName){
-    var ctx = document.getElementById(canvasName)
-    var myChart =new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: xAxis,
-          datasets: [
-            { 
-              data: yAxis,
-              pointRadius: 0,
-              label: "input",
-            }
-          ]
-        },
-        options: {
-            title: {
-                display: true,
-                text: title
+    function drawGraph(title, xAxis, yAxis, canvasName) {
+        var ctx = document.getElementById(canvasName)
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: xAxis,
+                datasets: [{
+                    data: yAxis,
+                    pointRadius: 0,
+                    label: "input",
+                }]
             },
-        },
-      })
-}
-document.getElementById("lapFuelText").innerHTML = `Fuel used this lap: ${fuelInLitres[0] - fuelInLitres[fuelInLitres.length-1]}`
-drawGraph("Throttle", time, throttle, "throttleCanvas")
-drawGraph("Brake", time, brake, "brakeCanvas")
-drawGraph("Fuel", time, fuelInLitres, "fuelCanvas")
+            options: {
+                title: {
+                    display: true,
+                    text: title
+                },
+            },
+        })
+    }
+    document.getElementById("lapFuelText").innerHTML = `Fuel used this lap: ${fuelInLitres[0] - fuelInLitres[fuelInLitres.length-1]}`
+    drawGraph("Throttle", time, throttle, "throttleCanvas")
+    drawGraph("Brake", time, brake, "brakeCanvas")
+    drawGraph("Fuel", time, fuelInLitres, "fuelCanvas")
+})
